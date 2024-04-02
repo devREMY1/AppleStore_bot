cab66a576586b79d972395a58f2b1535d98a2810
botREMY <andrii.maksyuta@gmail.com>
2024-04-02 20:53:33 +0300

    file-update

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const { spawn } = require("child_process");
 
 const app = express();
@@ -6,41 +7,28 @@ const port = 5500;
 
 app.use(express.json()); // Используем express.json() для обработки JSON данных
 
-app.use(express.static("public"));
-app.use(express.static(__dirname + "/public"));
+// Указываем путь к папке, содержащей статические файлы (HTML, CSS, JS, изображения и т. д.)
+app.use(express.static(path.join(__dirname, "public")));
 
+// Обработчик маршрута для главной страницы
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/templates/control.html");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Обработчики маршрутов для остальных страниц
+app.get("/category", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "category.html"));
+});
+
+app.get("/control", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "control.html"));
+});
+
+app.get("/admins", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "admins.html"));
 });
 
 let botProcess = null;
-
-function sendMessage() {
-  // Получаем текст сообщения из поля ввода
-  const message = document.getElementById("messageInput").value;
-  const chatId = "yourChatId"; // Замените "yourChatId" на реальный идентификатор чата
-
-  // Отправляем сообщение на сервер
-  fetch("/sendMessage", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ message: message, chatId: chatId }), // Отправляем оба поля: message и chatId
-  })
-    .then((response) => {
-      if (response.ok) {
-        console.log("Сообщение успешно отправлено");
-        // Очищаем поле ввода после успешной отправки
-        document.getElementById("messageInput").value = "";
-      } else {
-        console.error("Ошибка отправки сообщения");
-      }
-    })
-    .catch((error) => {
-      console.error("Ошибка сети:", error);
-    });
-}
 
 app.post("/command", (req, res) => {
   const command = req.body.command;
